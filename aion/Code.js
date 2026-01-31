b026cad2645ffeaf10287568203643e6d0ba315d
samidare20 <dolphine0019@gmail.com>
2026-01-31 20:30:20 +0900

    수식 도입, 오류 수정중

--- aion/Code.js
+++ aion/Code.js
@@ -1,3 +1,9 @@
+var lineName={};
+lineName["캐릭터 이름"]=3;
+lineName["남은 오드"]=4;
+lineName["충전시간 계산"]=5;
+lineName["추적시작 시간"]=25;
+
 function onEdit(e) {
   const sheet = e.source.getActiveSheet();
   const range = e.range;
@@ -6,12 +12,12 @@ function onEdit(e) {
   
   // B열(캐릭터 이름)에 새로운 값을 입력했을 때 -> 초기 세팅 자동화
   // 입력된 값(e.value)이 있을 때만 작동
-  if (col === 3 && row > 2 && e.value) { 
+  if (col === lineName["캐릭터 이름"] && row > 2 && e.value) { 
     // F열에 체크박스 생성
-    sheet.getRange(row, 6).insertCheckboxes();
+    sheet.getRange(row, lineName["충전시간 계산"]).insertCheckboxes();
   }
   // 변경된 곳이 2번째 열(C열, 체크박스)이고, 헤더(1행)가 아닌 경우만 실행
-  if (range.getColumn() === 6 && range.getRow() > 2) {
+  if (range.getColumn() === lineName["충전시간 계산"] && range.getRow() > 2) {
     const isChecked = range.getValue(); // 체크박스 상태 (true/false)
     const row = range.getRow();
     
@@ -19,14 +25,18 @@ function onEdit(e) {
     if (isChecked === true) {
       range.setValue(false); 
       
-      const existodd=parseInt(sheet.getRange(row,4).getValue());
-      const existticket=parseInt(sheet.getRange(row,5).getValue());
-      const remodd=((840-existodd)/15)*3;
-      const remticket=(21-existticket)*8;
-      
-      Logger.log(existodd+" "+existticket);
-      Logger.log(remodd+" "+remticket);
+      const existodd=parseInt(sheet.getRange(row,lineName["남은 오드"]).getValue());
+      Logger.log(existodd);
+      const r=String.fromCharCode(64+lineName["추적시작 시간"]);
+      sheet.getRange(row,lineName["추적시작 시간"]).setValue(new Date());
 
+      const formula=`=min(840,${existodd}+INT((INT(NOW())*24 + HOUR(NOW()) + 1)/3) - INT((INT(${r}${row})*24 + HOUR(${r}${row}) + 1)/3))`;
+
+      sheet.getRange(row,lineName["남은 오드"]).setFormula(formula);
+      Logger.log(formula);
+      /**
+      const remodd=((840-existodd)/15)*3;
+      
       var nexttime=new Date();
       const needtime=Math.min(remodd,remticket);
       nexttime.setHours(nexttime.getHours()+needtime);
@@ -38,7 +48,9 @@ function onEdit(e) {
       sheet.getRange(row,7).setValue(nexttime);
       sheet.getRange(row,8).setValue(Math.floor(endodd));
       sheet.getRange(row,9).setValue(Math.floor(enddticket));
-      sheet.getRange(row,10).setValue(Math.floor(Math.min(endodd/80,enddticket)));
+      sheet.getRange(row,10).setValue(Math.floor(Math.min(endodd/80,enddticket))); */
+
+
     }
   
   }
